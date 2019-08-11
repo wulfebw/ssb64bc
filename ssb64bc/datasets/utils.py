5feb839659a4b69e4b1543b146bddbed29aaccba
wulfebw <wulfebw@stanford.edu>
2019-08-10 22:54:14 -0700

    Add dataset classes

--- ssb64bc/datasets/utils.py
+++ ssb64bc/datasets/utils.py
@@ -6,10 +6,6 @@ _COLOR_IMAGE_MEAN = np.array([90, 150, 120]) / 255.0
 _GRAYSCALE_IMAGE_MEAN = np.array([120]) / 255.0
 
 
-def get_hdf5_transfrom():
-    return torchvision.transforms.ToTensor()
-
-
 def get_image_mean_std(image_type):
     if image_type == "color":
         return _COLOR_IMAGE_MEAN, [1, 1, 1]
@@ -27,7 +23,17 @@ def get_image_transforms(mean=_COLOR_IMAGE_MEAN, std=[1, 1, 1], resize=224, inte
         torchvision.transforms.Normalize(mean, std, inplace=True)
     ])
 
+
 def get_image_transforms_for_encoding(encoding, resize=224, interpolation=0):
     image_type = "color" if encoding == cv2.IMREAD_COLOR else "grayscale"
     mean, std = get_image_mean_std(image_type)
     return get_image_transforms(mean, std, resize, interpolation)
+
+
+def get_image_encoding(image_type):
+    if image_type == "color":
+        return cv2.IMREAD_COLOR
+    elif image_type == "grayscale":
+        return cv2.IMREAD_GRAYSCALE
+    else:
+        raise ValueError("invalid image type {}".format(args.image_type))
